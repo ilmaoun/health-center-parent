06e35e897788c035eb1c07fb8562fc2f4522422f
ilmaoun <ilmaoun@gmail.com>
2024-03-16 14:57:59 -0400

    Initial Commit

---
+++ src/main/java/com/helthcenter/parent/HelthCenterParentApplication.java
@@ -0,0 +1,13 @@
+package com.helthcenter.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelthCenterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelthCenterParentApplication.class, args);
+	}
+
+}
